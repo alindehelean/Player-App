7665da6987b5686ed6e28a654b725b0dabf90d92
Alin Dehelean <alyndehelean@gmail.com>
2020-05-09 16:29:58 +0300

    Task 3 almost done

--- classes/Player.js
+++ classes/Player.js
@@ -6,37 +6,36 @@ class Player {
   constructor() {
     this.div = Player.display();
     this.move();
-    this.hitWalls();
   }
 
 
   move() {
     console.log('miscare', this)
-    document.addEventListener('keyup', (event) => {
+    document.addEventListener('keyup',(event) => {
 
-      if (event.keyCode === 40) { 
+      if (event.keyCode === 40) {
         const oldTop = parseInt(this.div.style.top);
         this.div.style.top = `${oldTop + 20}px`;
         console.log(this.div.style.top);
-        // if(this.div.style.top === `388px`){
-        //   console.log("ba!");
-        //   event.keyCode === 0;
-        // }
-        this.hitWalls();
+        this.hitButtomWall();
+
       } else if (event.keyCode === 39) {
         const oldLeft = parseInt(this.div.style.left);
         this.div.style.left = `${oldLeft + 20}px`;
-        console.log(this.div.style.left);
+        // console.log(this.div.style.left);
+        this.hitRightWall();
       } else if (event.keyCode === 37) {
         const oldRight = parseInt(this.div.style.left)
         this.div.style.left = `${oldRight - 20}px`
-        console.log(this.div.style.left);
+        //console.log(this.div.style.left);
+        this.hitLeftWall();
       } else if (event.keyCode === 38) {
         const oldDown = parseInt(this.div.style.top);
         this.div.style.top = `${oldDown - 20}px`
-        console.log(this.div.style.top);
+        //console.log(this.div.style.top);
+        this.hitTopWall();
       }
-
+    
     })
   }
 
@@ -55,18 +54,36 @@ class Player {
 
     gameContainer.appendChild(div);
 
-
-
     return div;
 
   }
 
-  hitWalls(){
-    if(parseInt(this.div.style.top) === gameContainer.offsetWidth - 20){
-        console.log(parseInt(this.div.style.top));
-        console.log('ai trecut limita')
+  hitButtomWall() {
+    if (parseInt(this.div.style.top) === gameContainer.offsetHeight ) {
+      console.log('ai trecut limita de jos')
+      alert("STOP !")
     }
-    console.log(document.querySelector('#gameContainer').offsetWidth);
+  }
+
+  hitTopWall() {
+    if (parseInt(this.div.style.top) === -20) {
+      console.log('ai trecut limita de sus')
+      alert("STOP !")
+    }
+  }
+
+  hitLeftWall() {
+    if (parseInt(this.div.style.left) === -20) {
+      console.log('ai trecut limita din stanga')
+      alert("STOP !")
+    }
+  }
+
+  hitRightWall() {
+    if (parseInt(this.div.style.left) === gameContainer.offsetWidth) {
+      console.log('ai trecut limita din dreapta')
+      alert("STOP !")
+    }
   }
 
 }
